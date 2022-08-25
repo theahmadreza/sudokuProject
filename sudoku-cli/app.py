329810d9e3768d9c88b8a7961022b3fcd9ced21e
itsahmadrezza <111778540+theahmadreza@users.noreply.github.com>
2022-08-25 22:36:00 +0430

    Add function get  info, little code clining

--- sudoku-cli/app.py
+++ sudoku-cli/app.py
@@ -1,17 +1,35 @@
 from dokusan import generators
 from subprocess import call
 import numpy as np
+import getpass
 
+
+# function for clear terminal after each answer
 def clear():
     rc = call("./clear.sh")
 
+
+# function for get data from db
+def getInfo():
+    pass
+    userName = input("your Username: ")
+    password = getpass.getpass(prompt='your Password: ')
+
+    if password.lower() == PASSWORD:
+        login = True
+    else:
+        login = False
+
+
+# core of project
 arr = np.array(list(str(generators.random_sudoku(avg_rank=150))))
-    
+
 print(arr.reshape(9,9))
 
 
 a = True
 
+# while loggin == True
 while True:
     
     if a == True:
@@ -26,6 +44,6 @@ while True:
 
         # core
         arr[row] = index
-        clear()
+        # clear()
         print(arr.reshape(9,9))
         
